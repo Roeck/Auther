3e3277ff857f03e68b3a2402da9c1942876497c7
Roeck <rons9n@gmail.com>
2021-05-29 17:03:28 -0400

    Users index and show pages/routes added

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { confirmations: 'confirmations' }
+
+  resources :users, only: [:index, :show]
+  
   root 'static_pages#home'
   get "privacy_policy", to: "static_pages#privacy_policy"
 end
