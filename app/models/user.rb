3170218b8c5047c9f6880d09beb051994e90217b
Roeck <rons9n@gmail.com>
2021-05-29 18:52:02 -0400

    Devise lockable account added

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
-         :confirmable, :trackable
+         :confirmable, :trackable, :lockable
 end
