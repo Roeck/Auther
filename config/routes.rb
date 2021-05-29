ff5155cbf387408180aeb5c69e9eb7a1076a747b
Roeck <rons9n@gmail.com>
2021-05-29 16:34:48 -0400

    Confirmations controller added for redirecting user to root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { confirmations: 'confirmations' }
   root 'static_pages#home'
   get "privacy_policy", to: "static_pages#privacy_policy"
 end
