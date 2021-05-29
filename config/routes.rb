781df1c5c9717a23c1472a61a8a3d9af59bd77f7
Roeck <rons9n@gmail.com>
2021-05-29 18:26:45 -0400

    Delete user funcionality done

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { confirmations: 'confirmations' }
 
-  resources :users, only: [:index, :show]
-  
+  resources :users, only: [:index, :show, :destroy]
+
   root 'static_pages#home'
   get "privacy_policy", to: "static_pages#privacy_policy"
 end
